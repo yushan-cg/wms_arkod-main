7d13f2ab4a92aaad40c7e9fff713747fabb7ef5d
imyushan <132455320+yushan-cg@users.noreply.github.com>
2024-05-13 17:18:10 +0800

    add customer MCR

---
+++ app/Http/Controllers/backend/CustomerController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
+
+class CustomerController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        // Get the currently authenticated user
+        $user = auth()->user();
+
+        // Get the customers that belong to the user
+        $customers = Customer::where('user_id', $user->id)->get();
+
+        // Return the view with the companies data
+        return view('backend.customer.detail_customer', compact('customers'));
+
+    }
+
+    public function getUsers()
+    {
+        $users = User::all();
+    
+        return response()->json($users);
+    }
+
+    public function showAll()
+    {
+        $customers = Customer::all();
+
+        return view('backend.customer.customer_list', compact('customers'));
+    }
+            
+} 
